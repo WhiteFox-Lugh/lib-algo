eb1f1ea7eac41d836f9e57d278fb50ac9bc9bebb
Arthur-Lugh <38004439+WhiteFox-Lugh@users.noreply.github.com>
2021-03-23 22:00:24 +0900

    solve: NTL(wip1)

---
+++ aoj/NTL/1_D.cpp
@@ -0,0 +1,137 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fst first
+#define snd second
+#define mp make_pair
+#define ALL(obj) (obj).begin(), (obj).end()
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define RFOR(i, a, b) for (int i = (b - 1); i >= a; i--)
+#define REP(i, n) FOR(i, 0, n)
+#define RREP(i, n) RFOR(i, 0, n)
+#define SIZE(x) ((int)(x).size())
+#define debug(x) \
+  cerr << #x << " -> " << x << " (line:" << __LINE__ << ")" << '\n';
+#define debugpair(x, y)                                         \
+  cerr << "(" << #x << ", " << #y << ") -> (" << x << ", " << y \
+       << ") (line:" << __LINE__ << ")" << '\n';
+typedef long long lint;
+typedef pair<int, int> pint;
+typedef pair<lint, lint> plint;
+typedef vector<lint> vec;
+typedef vector<vector<lint>> matrix;
+typedef priority_queue<lint> p_que;
+typedef priority_queue<lint, vector<lint>, greater<lint>> p_que_rev;
+const lint INF = INT_MAX;
+const lint LINF = LLONG_MAX;
+const lint MOD = 1000000000 + 7;
+const double EPS = 1e-9;
+const double PI = acos(-1);
+const int di[]{0, -1, 0, 1, -1, -1, 1, 1};
+const int dj[]{1, 0, -1, 0, 1, -1, -1, 1};
+
+lint gcd(lint a, lint b) {
+  assert(a > 0 && b > 0);
+  lint r;
+  while (b != 0) {
+    r = a % b;
+    a = b;
+    b = r;
+  }
+  return a;
+}
+
+lint gcd_vec(vector<lint> vec) {
+  int len = vec.size();
+  lint res = vec[0];
+  for (int i = 1; i < len; ++i) {
+    res = gcd(res, vec[i]);
+  }
+  return res;
+}
+
+lint lcm(lint a, lint b) {
+  assert(a > 0 && b > 0);
+  return (a / gcd(a, b)) * b;
+}
+
+lint lcm_vec(vector<lint> vec) {
+  int len = vec.size();
+  lint res = vec[0];
+  for (int i = 1; i < len; ++i) {
+    res = lcm(res, vec[i]);
+  }
+  return res;
+}
+
+lint power(lint x, lint n, lint mod = MOD) {
+  lint ret = 1;
+  while (n > 0) {
+    if (n & 1) {
+      (ret *= x) %= mod;
+    }
+    (x *= x) %= mod;
+    n >>= 1;
+  }
+  return ret;
+}
+
+vector<lint> make_power(int n, lint base) {
+  lint num = 1;
+  vector<lint> ret;
+  for (int i = 0; i <= n; ++i) {
+    ret.push_back(num);
+    num *= base;
+  }
+  return ret;
+}
+
+pair<vector<lint>, vector<lint>> prime_factor(lint n) {
+  assert(n > 0);
+  vector<lint> prime_num, cnt;
+  lint m = n;
+  for (lint i = 2; i * i <= n; i++) {
+    if (m % i != 0) {
+      continue;
+    }
+    lint c = 0;
+    while (m % i == 0) {
+      c++;
+      m /= i;
+    }
+    prime_num.push_back(i);
+    cnt.push_back(c);
+  }
+  if (m > 1) {
+    prime_num.push_back(m);
+    cnt.push_back(1);
+  }
+  return make_pair(prime_num, cnt);
+}
+
+// vars
+lint n, ans;
+
+void input() { cin >> n; }
+
+void output() { cout << ans << endl; }
+
+lint euler_phi(lint n) {
+  lint res = n;
+  auto pf = prime_factor(n);
+  for (auto prime_num : pf.fst) {
+    res *= prime_num - 1;
+    res /= prime_num;
+  }
+  return res;
+}
+
+void solve() { ans = euler_phi(n); }
+
+int main() {
+  cin.tie(0);
+  ios_base::sync_with_stdio(false);
+  input();
+  solve();
+  output();
+  return 0;
+}
